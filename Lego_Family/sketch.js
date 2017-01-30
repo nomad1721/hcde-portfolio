deda45b642d32d7c0d0123b38d21061a6c79a08b
nomad1721 <jmbhcde@uw.edu>
2017-01-30 15:52:04 -0800

    updates to assignment

--- Lego_Family/sketch.js
+++ Lego_Family/sketch.js
@@ -3,12 +3,21 @@
 // Progam that animates characters from Wall-E movie (in the style of a Lego family)
 
 
-var x = 600 // define starting point of x variable
-var y = 372 // define starting point of y variable
-var dir = -1
+var x = 600 // define starting x coordinate - Wall-E
+var y = 372 // define starting y coordinate - Wall-E
+var e = 680 // define starting x coordinate - Eve
+var v = 356 // define starting y coordinate - Eve
+var m = 760 // define starting x corrdinate - Moe
+var o = 356 // define starting y coordinate - Moe
+var p = 840 // define starting x coordinate - Plant
+var l = 372 // define starting y coordinate - Plant
+
+var dirx = -1
+var diry = 1
+
 
 function setup() {
-  createCanvas(1800, 900); // determimes canvas size
+  createCanvas(1500, 900); // determimes canvas size
   background(206, 233, 244); // sets background color
   noStroke(); // disables stroke
 }
@@ -17,22 +26,57 @@ function draw() {
   console.log(frameRate()); // displays frame rate in console
   background(206, 233, 244);  // sets background color
   walle(); // draws 'walle' fxn
-  walleeyes(); // draws "walleeyes" fxn
   eve(); // draws "eve" fxn
-  eveeyes(); // draws "eveeyes" fxn
   moe(); // draws "moe" fxn
-  moeeyes(); // draws "moeeyes" fxn
   plant(); // draws "plant" fxn
   
-  if (x > 600 || x < 25) { // sets range of allowed movemnet on x axis
-  dir = dir * -1; // sets direction of movement 
-  }
-  x = x + dir  
-  if (y > 372 || y < 0){
-    dir = dir * -1
-  }
-  y = y + dir
+  // Wall-E movement ---------------------------------------
   
+  if (x > 600 || x < 25) { // sets range of allowed movemnet on x axis for WallE
+    dirx = dirx * -1; // sets direction of movement 
+  }
+  x = max(x + dirx, 25)  // sets maximum value
+  
+  if (y > 372 || y < 25){
+    dirx = dirx * -1;
+  }
+  y = max(y + dirx, 25)
+  
+  // Eve movement ------------------------------------------
+  
+  if (e > 680 || e < 25 ){
+    dirx = dirx * -1;
+  }
+  e = max(x + dirx, 25)
+
+  if (v > 356 || v < 25){
+    diry = diry;
+  }
+  v = min(v + diry, 720)
+  
+  // Moe movement -------------------------------------------
+  
+  if (m > 760 || m < 1500){
+    diry = diry;
+  }
+  m = min(m + diry, 1350)
+  
+  if (o > 356 || o < 25){
+    diry = diry;
+  }
+  o = min(o + diry, 720)
+  
+// Plant movement ------------------------------------------ 
+ 
+  if (p > 840 || p < 1500){
+    diry = diry;
+  }
+  p = min(m + diry, 1350)
+  
+  if (l > 372 || l < 25){
+    dirx = dirx;
+  }
+  l = min(l + dirx, 720)
 }
 
 // begin "Wall-E" fxns ----------------------------
@@ -57,6 +101,8 @@ function walle() { // elements that make up Wall-E character
   // red spot
   fill(255, 0, 0); // sets fill color
   ellipse(x + 56, y + 104, 12, 12); // draws an ellipse
+  
+  walleeyes(); // draws "walleeyes" fxn
 }
 
 function walleeyes() { // calls "walleeye" fxn
@@ -72,28 +118,32 @@ function walleeye(x) { // parameters for single eye
 // begin "Eve" fxns ------------------------------
 
 function eve() { // elements that make up Eve character
+ 
   // head
   fill(255); // sets fill color
-  rect(x + 80, 356, 64, 48); // draws a rectangle
+  rect(e, v, 64, 48); // draws a rectangle
   
   // visor
   fill(0); // sets fill color
-  rect(x + 80, 372, 64, 16); // draws a rectangle
+  rect(e, v + 16, 64, 16); // draws a rectangle
   
   // body
   fill(255); // sets fill color
-  rect(x + 80, 420, 64, 80); // draws a rectangle
+  rect(e, v + 64, 64, 80); // draws a rectangle
+  
+  eveeyes(); // draws "eveeyes" fxn
 }
 
 function eveeyes () { // draws two eyes
-  eveeye(x + 86); // determines x coordinate of eye
-  eveeye(x + 120); // determines x coordinate of second eye
+  eveeye(e + 6); // determines x coordinate of eye
+  eveeye(e + 40); // determines x coordinate of second eye
 }
 
-function eveeye(x) { // parameters for eye
+function eveeye(e) { // parameters for eye
   fill(81, 171, 246); // sets fill color
-  rect(x, 378, 20, 4); // draws a rectangle
+  rect(e, v + 22, 20, 4); // draws a rectangle
 }
+
 
 // begin "Moe" fxn ---------------------------------
 
@@ -101,36 +151,38 @@ function moe() { // elements that make up Moe character
   
   // light
   fill(235, 164, 59); // sets fill color
-  rect(776, 348, 32, 16); // draws a rectangle
+  rect(m + 16, o - 8, 32, 16); // draws a rectangle
   
   // head
   fill(244, 244, 242); // sets fill color
-  rect(760, 364, 64, 48); // draws a rectangle
+  rect(m, o + 8, 64, 48); // draws a rectangle
  
   // body
-  rect(760, 420, 64, 64); // draws a rectangle
+  rect(m, o + 64, 64, 64); // draws a rectangle
  
   // visor
   fill(0); // sets fill color
-  rect(776, 396, 32, 16); // draws a rectangle
+  rect(m + 16, o + 40, 32, 16); // draws a rectangle
   
   // neck
   fill(53, 49, 48); // sets fill color
-  rect(784, 412, 16, 8); // draws a rectangle
+  rect(m + 24, o + 56, 16, 8); // draws a rectangle
  
   // treads
   fill(0); // sets fill color
-  rect(776, 484, 32, 16); // draws a rectangle
+  rect(m + 16, o + 128, 32, 16); // draws a rectangle
+  
+  moeeyes(); // draws "moeeyes" fxn
 }
 
 function moeeyes() { // draws two eyes
-  moeeye(782); // determines x coordinate of first eye
-  moeeye(794); // determines x coordinate of second eye
+  moeeye(m + 22); // determines x coordinate of first eye
+  moeeye(m + 34); // determines x coordinate of second eye
 }
 
-function moeeye(x) { // sets parameters for one eye
+function moeeye(m) { // sets parameters for one eye
   fill(235, 164, 59); // sets fill color
-  rect(x, 402, 8, 2); // draws a rectangle
+  rect(m, o + 46, 8, 2); // draws a rectangle
 }
 
 // begin "Plant" fxns -------------------------------
@@ -139,12 +191,12 @@ function plant() { // elements that make up Plant character
   
   // boot
   fill(99, 63, 35); // sets fill color
-  rect(840, 468, 48, 16); // draws a rectangle
-  rect(840, 484, 64, 16); // draws a rectangle
+  rect(p, l + 96, 48, 16); // draws a rectangle
+  rect(p, l + 112, 64, 16); // draws a rectangle
   
   // stem
   fill(57, 181, 74); // sets fill color
-  rect(856, 372, 16, 96); // draws a rectangle
-  rect(840, 388, 64, 16); // draws a rectangle
-  rect(840, 420, 32, 16); // draws a rectangle
+  rect(p + 16, l, 16, 96); // draws a rectangle
+  rect(p, l + 16, 64, 16); // draws a rectangle
+  rect(p, l + 48, 32, 16); // draws a rectangle
 }
