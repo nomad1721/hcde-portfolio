2559775263b07e3c60d4909109f4778c0ac951c1
nomad1721 <jmbhcde@uw.edu>
2017-02-26 18:30:24 -0800

    upload to github

---
+++ custom_fonts/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(500,500);
+  textSize(50);
+  textFont("Raleway");
+}
+
+function draw() {
+  background(255);
+  text("Hello", 100, 100); // position is based on text baseline
+}
