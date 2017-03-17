e11ea1f86e6ec69e3b22431abc0ecf17c19e1166
nomad1721 <jmbhcde@uw.edu>
2017-03-17 16:56:04 -0700

    made changes to program code & cleaned things up

--- Lego_Family/sketch.js
+++ Lego_Family/sketch.js
@@ -1,202 +1,144 @@
-// Josh Baker
-// edited 1/28/16
-// Progam that animates characters from Wall-E movie (in the style of a Lego family)
+/*  
+    Created By:   Josh Baker
+    Description:  Progam that animates characters from Wall-E movie. They move from the four corners to center of the canvas and stop (in the style of a Lego family).
+    Last Edit:    03/17/2017
+*/
 
+var walleX = 25 // define starting x coordinate - Wall-E
+var walleY = 75 // define starting y coordinate - Wall-E
+var eveX = 25 // define starting x coordinate - Eve
+var eveY = 600 // define starting y coordinate - Eve
+var moeX = 1125 // define starting x corrdinate - Moe
+var moeY = 600 // define starting y coordinate - Moe
+var plantX = 1125 // define starting x coordinate - Plant
+var plantY = 75 // define starting y coordinate - Plant
+var dir = 2 // sets initial speed of movement of characters
 
-var walleX = 600 // define starting x coordinate - Wall-E
-var walleY = 372 // define starting y coordinate - Wall-E
-var eveX = 680 // define starting x coordinate - Eve
-var eveY = 356 // define starting y coordinate - Eve
-var moeX = 760 // define starting x corrdinate - Moe
-var moeY = 356 // define starting y coordinate - Moe
-var plantX = 840 // define starting x coordinate - Plant
-var plantY = 372 // define starting y coordinate - Plant
-
-var dirx = -1
-var diry = 1
-
-
-function setup() {
-  createCanvas(1500, 900); // determimes canvas size
+function setup() { // calls intital setup function - this code only runs once at the start of the program
+  createCanvas(1200, 800); // determimes canvas size
   background(206, 233, 244); // sets background color
   noStroke(); // disables stroke
 }
 
-function draw() {
-  console.log(frameRate()); // displays frame rate in console
+function draw() { // determines what happens in the draw function - this code repeats until the program stops
   background(206, 233, 244);  // sets background color
   walle(); // draws 'walle' fxn
   eve(); // draws "eve" fxn
   moe(); // draws "moe" fxn
   plant(); // draws "plant" fxn
   
-  // Wall-E movement ---------------------------------------
+  // Wall-E movement ------------------------------------------------------------------
+  walleX = min(walleX + (dir + 3), 425); // moves Wall-E along X axis towards center position and stops
+  walleY = min(walleY + (dir + 3), 350); // moves Wall-E along Y axis towards center position and stops
   
-  if (walleX > 600 || walleX < 25) { // sets range of allowed movemnet on x axis for WallE
-    dirx = dirx * -1; // sets direction of movement 
-  }
-  walleX = max(walleX + dirx, 25)  // sets maximum value
+  // Eve movement ---------------------------------------------------------------------
+  eveX = min(eveX + (dir + 3), 505); // moves Eve along X axis towards center position and stops
+  eveY = max(eveY - (dir + 3), 335); // moves Eve along y axis towards center position and stops  
   
-  if (walleY > 372 || walleY < 25){
-    dirx = dirx * -1;
-  }
-  walleY = max(walleY + dirx, 25)
+  // Moe movement ---------------------------------------------------------------------
+  moeX = max(moeX - (dir + 1.25), 585); // moves Moe along X axis towards center position and stops
+  moeY = max(moeY - (dir + 1.25), 335); // moves Moe along Y axis towards center position and stops 
   
-  // Eve movement ------------------------------------------
-  
-  if (eveX > 680 || eveX < 25 ){
-    dirx = dirx * -1;
-  }
-  eveX = max(eveX + dirx, 25)
-
-  if (eveY > 356 || eveY < 25){
-    diry = diry;
-  }
-  eveY = min(eveY + diry, 720)
-  
-  // Moe movement -------------------------------------------
-  
-  if (moeX > 760 || moeX < 1500){
-    diry = diry;
-  }
-  moeX = min(moeX + diry, 1350)
-  
-  if (moeY > 356 || moeY < 25){
-    diry = diry;
-  }
-  moeY = min(moeY + diry, 720)
-  
-// Plant movement ------------------------------------------ 
- 
-  if (plantX > 840 || plantX < 1500){
-    diry = diry;
-  }
-  plantX = min(plantX + diry, 1350)
-  
-  if (plantY > 372 || plantY < 25){
-    dirx = dirx;
-  }
-  plantY = max(plantY + dirx, 25)
+  // Plant movement --------------------------------------------------------------------- 
+  plantX = max(plantX - dir, 665); // moves Plant along X axis towards center position and stops
+  plantY = min(plantY + dir, 350); // moves Plant along Y axis towards center position and stops
 }
 
-// begin "Wall-E" fxns ----------------------------
-
-function walle() { // elements that make up Wall-E character
-  // head
-  fill(86, 86, 84); // sets fill color
-  rect(walleX, walleY, 64, 32); // draws a rectangle
-  
-  // neck
-  fill(68, 67, 65); // sets fill color
-  rect(walleX + 24, walleY + 32, 16, 16); // draws a rectangle
-  
-  // body
-  fill(235, 164, 59); // sets fill color
-  rect(walleX, walleY + 48, 64, 64); // draws a rectangle
-  
-  // treads
-  fill(53, 49, 48); // sets fill color
-  rect(walleX, walleY + 112, 64, 16); // draws a rectangle
-  
-  // red spot
-  fill(255, 0, 0); // sets fill color
-  ellipse(walleX + 56, walleY + 104, 12, 12); // draws an ellipse
-  
-  walleeyes(); // draws "walleeyes" fxn
+// begin "Wall-E" fxns ================================================================
+function walle() { // elements that make up Wall-E character --------------------------
+  // head -----------------------------------
+  fill(86, 86, 84);
+  rect(walleX, walleY, 64, 32);
+  // neck ----------------------------------
+  fill(68, 67, 65);
+  rect(walleX + 24, walleY + 32, 16, 16);
+  // body ----------------------------------
+  fill(235, 164, 59);
+  rect(walleX, walleY + 48, 64, 64);
+  // treads --------------------------------
+  fill(53, 49, 48);
+  rect(walleX, walleY + 112, 64, 16);
+  // red spot ------------------------------
+  fill(255, 0, 0);
+  ellipse(walleX + 56, walleY + 104, 12, 12);
+  walleEyes();
 }
 
-function walleeyes() { // calls "walleeye" fxn
-  walleeye(walleX + 16); // sets location of "walleeye" fxn
-  walleeye(walleX + 48); // sets location of second "walleeye" fxn
+function walleEyes() { // calls "walleEye" fxn ---------------------------------------
+  walleEye(walleX + 16); 
+  walleEye(walleX + 48);
 }
 
-function walleeye(walleX) { // parameters for single eye
-  fill(0); // set fill color
-  ellipse(walleX, walleY + 16, 25, 25); // draws an ellipse
+function walleEye(walleX) { // parameters for single eye -----------------------------
+  fill(0);
+  ellipse(walleX, walleY + 16, 25, 25);
 }
 
-// begin "Eve" fxns ------------------------------
-
-function eve() { // elements that make up Eve character
- 
-  // head
-  fill(255); // sets fill color
-  rect(eveX, eveY, 64, 48); // draws a rectangle
-  
-  // visor
-  fill(0); // sets fill color
-  rect(eveX, eveY + 16, 64, 16); // draws a rectangle
-  
-  // body
-  fill(255); // sets fill color
-  rect(eveX, eveY + 64, 64, 80); // draws a rectangle
-  
-  eveeyes(); // draws "eveeyes" fxn
+// begin "Eve" fxns ===================================================================
+function eve() { // elements that make up Eve character -------------------------------
+  // head --------------------------
+  fill(255);
+  rect(eveX, eveY, 64, 48);
+  // visor -------------------------
+  fill(0);
+  rect(eveX, eveY + 16, 64, 16);
+  // body --------------------------
+  fill(255);
+  rect(eveX, eveY + 64, 64, 80); 
+  eveEyes(); 
 }
 
-function eveeyes () { // draws two eyes
-  eveeye(eveX + 6); // determines x coordinate of eye
-  eveeye(eveX + 40); // determines x coordinate of second eye
+function eveEyes () { // calls "eveEye" function --------------------------------------
+  eveEye(eveX + 6); 
+  eveEye(eveX + 40);
 }
 
-function eveeye(eveX) { // parameters for eye
-  fill(81, 171, 246); // sets fill color
-  rect(eveX, eveY + 22, 20, 4); // draws a rectangle
+function eveEye(eveX) { // parameters for single eye ----------------------------------
+  fill(81, 171, 246);
+  rect(eveX, eveY + 22, 20, 4);
 }
 
-
-// begin "Moe" fxn ---------------------------------
-
-function moe() { // elements that make up Moe character
-  
-  // light
-  fill(235, 164, 59); // sets fill color
-  rect(moeX + 16, moeY - 8, 32, 16); // draws a rectangle
-  
-  // head
-  fill(244, 244, 242); // sets fill color
-  rect(moeX, moeY + 8, 64, 48); // draws a rectangle
- 
-  // body
-  rect(moeX, moeY + 64, 64, 64); // draws a rectangle
- 
-  // visor
-  fill(0); // sets fill color
-  rect(moeX + 16, moeY + 40, 32, 16); // draws a rectangle
-  
-  // neck
-  fill(53, 49, 48); // sets fill color
-  rect(moeX + 24, moeY + 56, 16, 8); // draws a rectangle
- 
-  // treads
-  fill(0); // sets fill color
-  rect(moeX + 16, moeY + 128, 32, 16); // draws a rectangle
-  
-  moeeyes(); // draws "moeeyes" fxn
+// begin "Moe" fxns ====================================================================
+function moe() { // elements that make up Moe character --------------------------------
+  // light ---------------------------
+  fill(235, 164, 59);
+  rect(moeX + 16, moeY - 8, 32, 16);
+  // head ----------------------------
+  fill(244, 244, 242);
+  rect(moeX, moeY + 8, 64, 48);
+  // body ----------------------------
+  rect(moeX, moeY + 64, 64, 64);
+  // visor ---------------------------
+  fill(0);
+  rect(moeX + 16, moeY + 40, 32, 16);
+  // neck ----------------------------
+  fill(53, 49, 48);
+  rect(moeX + 24, moeY + 56, 16, 8);
+  // treads --------------------------
+  fill(0);
+  rect(moeX + 16, moeY + 128, 32, 16);
+  moeEyes();
 }
 
-function moeeyes() { // draws two eyes
-  moeeye(moeX + 22); // determines x coordinate of first eye
-  moeeye(moeX + 34); // determines x coordinate of second eye
+function moeEyes() { // calls "moeEye" function ---------------------------------------
+  moeEye(moeX + 22);
+  moeEye(moeX + 34);
 }
 
-function moeeye(moeX) { // sets parameters for one eye
-  fill(235, 164, 59); // sets fill color
-  rect(moeX, moeY + 46, 8, 2); // draws a rectangle
+function moeEye(moeX) { // sets parameters for single eye -----------------------------
+  fill(235, 164, 59);
+  rect(moeX, moeY + 46, 8, 2);
 }
 
-// begin "Plant" fxns -------------------------------
-
-function plant() { // elements that make up Plant character
-  
-  // boot
-  fill(99, 63, 35); // sets fill color
-  rect(plantX, plantY + 96, 48, 16); // draws a rectangle
-  rect(plantX, plantY + 112, 64, 16); // draws a rectangle
-  
-  // stem
-  fill(57, 181, 74); // sets fill color
-  rect(plantX + 16, plantY, 16, 96); // draws a rectangle
-  rect(plantX, plantY + 16, 64, 16); // draws a rectangle
-  rect(plantX, plantY + 48, 32, 16); // draws a rectangle
+// begin "Plant" fxn ==================================================================
+function plant() { // elements that make up Plant character ---------------------------
+  // boot ----------------------------
+  fill(99, 63, 35);
+  rect(plantX, plantY + 96, 48, 16);
+  rect(plantX, plantY + 112, 64, 16);
+  // stem ----------------------------
+  fill(57, 181, 74);
+  rect(plantX + 16, plantY, 16, 96);
+  rect(plantX, plantY + 16, 64, 16);
+  rect(plantX, plantY + 48, 32, 16);
 }
